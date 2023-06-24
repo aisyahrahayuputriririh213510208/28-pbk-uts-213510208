2ad73fd299bad68d077fa1f0ed8cb10efd9f841f
aisyahrahayuputriririh213510208 <131292827+aisyahrahayuputriririh213510208@users.noreply.github.com>
2023-06-24 08:33:58 +0700

    Add files via upload

---
+++ src/store.js
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+import { createPinia } from 'pinia';
+import router from './router';
+
+const piniaInstance = createPinia();
+
+export const useTodoStore = defineStore('todo', {
+  state: () => ({
+    todos: [],
+  }),
+  getters: {
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed);
+    },
+    incompleteTodos() {
+      return this.todos.filter(todo => !todo.completed);
+    },
+  },
+  actions: {
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    },
+    toggleTodoCompletion(todo) {
+      todo.completed = !todo.completed;
+    },
+  },
+});
+
+// Attach the Pinia instance to Vue Router navigation guards
+router.beforeEach((to, from, next) => {
+  piniaInstance.router = router;
+  next();
+});
+
+export default piniaInstance;
